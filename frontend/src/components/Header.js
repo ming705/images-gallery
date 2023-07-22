e0ad62ab11574ea5fedef91255111f3609b4e529
Mingsheng Xie <mingsheng.xie@gmail.com>
2023-07-22 18:08:24 -0400

    frontend and api get service fully functional

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
-const Header = (props) => {
-    return (
-        <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
-      </Navbar>
-    )
+const navbarStyle = {
+  backgroundColor: '#eeeeee',
+};
+
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
